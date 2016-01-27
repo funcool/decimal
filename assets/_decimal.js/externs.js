94a443490c87f8d92665a1da9edfa140c377ad9f
Andrey Antukh <niwi@niwi.nz>
2016-01-27 19:28:26 +0200

    Add complete externs for fully compatibility with advanced compilation modes.

---
+++ assets/_decimal.js/externs.js
@@ -0,0 +1,775 @@
+/**
+ * @const
+ * @constructor
+ */
+var Decimal = function() {};
+
+/**
+ * @this {Decimal}
+ * @return {Decimal}
+ */
+Decimal.prototype.absoluteValue = function() {};
+
+/**
+ * @this {Decimal}
+ * @return {Decimal}
+ */
+Decimal.prototype.abs = function() {};
+
+/**
+ * @this {Decimal}
+ * @return {Decimal}
+ */
+Decimal.prototype.ceil = function() {};
+
+/**
+ * @this {Decimal}
+ * @return {number|null}
+ */
+Decimal.prototype.compareTo = function() {};
+
+/**
+ * @this {Decimal}
+ * @return {number|null}
+ */
+Decimal.prototype.cmp = function() {};
+
+/**
+ * @this {Decimal}
+ * @return {Decimal}
+ */
+Decimal.prototype.cosine = function() {};
+
+/**
+ * @this {Decimal}
+ * @return {Decimal}
+ */
+Decimal.prototype.cos = function() {};
+
+/**
+ * @this {Decimal}
+ * @return {Decimal}
+ */
+Decimal.prototype.cubeRoot = function() {};
+
+/**
+ * @this {Decimal}
+ * @return {Decimal}
+ */
+Decimal.prototype.cbrt = function() {};
+
+/**
+ * @this {Decimal}
+ * @return {number}
+ */
+Decimal.prototype.decimalPlaces = function() {};
+
+/**
+ * @this {Decimal}
+ * @return {number}
+ */
+Decimal.prototype.dp = function() {};
+
+
+/**
+ * @this {Decimal}
+ * @return {Decimal}
+ */
+Decimal.prototype.dividedBy = function() {};
+
+/**
+ * @this {Decimal}
+ * @return {Decimal}
+ */
+Decimal.prototype.div = function() {};
+
+/**
+ * @this {Decimal}
+ * @return {Decimal}
+ */
+Decimal.prototype.dividedToIntegerBy = function() {};
+
+/**
+ * @this {Decimal}
+ * @return {Decimal}
+ */
+Decimal.prototype.divToInt = function() {};
+
+/**
+ * @this {Decimal}
+ * @return {Boolean}
+ */
+Decimal.prototype.equals = function() {};
+
+/**
+ * @this {Decimal}
+ * @return {Boolean}
+ */
+Decimal.prototype.eq = function() {};
+
+/**
+ * @this {Decimal}
+ * @return {Decimal}
+ */
+Decimal.prototype.floor = function() {};
+
+/**
+ * @this {Decimal}
+ * @return {Boolean}
+ */
+Decimal.prototype.greaterThan = function() {};
+
+/**
+ * @this {Decimal}
+ * @return {Boolean}
+ */
+Decimal.prototype.gt = function() {};
+
+/**
+ * @this {Decimal}
+ * @return {Boolean}
+ */
+Decimal.prototype.greaterThanOrEqualTo = function() {};
+
+/**
+ * @this {Decimal}
+ * @return {Boolean}
+ */
+Decimal.prototype.gte = function() {};
+
+/**
+ * @this {Decimal}
+ * @return {Decimal}
+ */
+Decimal.prototype.hyperbolicCosine = function() {};
+
+/**
+ * @this {Decimal}
+ * @return {Decimal}
+ */
+Decimal.prototype.cosh = function() {};
+
+/**
+ * @this {Decimal}
+ * @return {Decimal}
+ */
+Decimal.prototype.hyperbolicSine = function() {};
+
+/**
+ * @this {Decimal}
+ * @return {Decimal}
+ */
+Decimal.prototype.sinh = function() {};
+
+/**
+ * @this {Decimal}
+ * @return {Decimal}
+ */
+Decimal.prototype.hyperbolicTangent = function() {};
+
+/**
+ * @this {Decimal}
+ * @return {Decimal}
+ */
+Decimal.prototype.tanh = function() {};
+
+/**
+ * @this {Decimal}
+ * @return {Decimal}
+ */
+Decimal.prototype.inverseCosine = function() {};
+
+/**
+ * @this {Decimal}
+ * @return {Decimal}
+ */
+Decimal.prototype.acos = function() {};
+
+/**
+ * @this {Decimal}
+ * @return {Decimal}
+ */
+Decimal.prototype.inverseHyperbolicCosine = function() {};
+
+/**
+ * @this {Decimal}
+ * @return {Decimal}
+ */
+Decimal.prototype.acosh = function() {};
+
+/**
+ * @this {Decimal}
+ * @return {Decimal}
+ */
+Decimal.prototype.inverseHyperbolicSine = function() {};
+
+/**
+ * @this {Decimal}
+ * @return {Decimal}
+ */
+Decimal.prototype.asinh = function() {};
+
+/**
+ * @this {Decimal}
+ * @return {Decimal}
+ */
+Decimal.prototype.inverseHyperbolicTangent = function() {};
+
+/**
+ * @this {Decimal}
+ * @return {Decimal}
+ */
+Decimal.prototype.atanh = function() {};
+
+/**
+ * @this {Decimal}
+ * @return {Decimal}
+ */
+Decimal.prototype.inverseSine = function() {};
+
+/**
+ * @this {Decimal}
+ * @return {Decimal}
+ */
+Decimal.prototype.asin = function() {};
+
+/**
+ * @this {Decimal}
+ * @return {Decimal}
+ */
+Decimal.prototype.inverseTangent = function() {};
+
+/**
+ * @this {Decimal}
+ * @return {Decimal}
+ */
+Decimal.prototype.atan = function() {};
+
+/**
+ * @this {Decimal}
+ * @return {Boolean}
+ */
+Decimal.prototype.isFinite = function() {};
+
+/**
+ * @this {Decimal}
+ * @return {Boolean}
+ */
+Decimal.prototype.isInteger = function() {};
+
+/**
+ * @this {Decimal}
+ * @return {Boolean}
+ */
+Decimal.prototype.isInt = function() {};
+
+/**
+ * @this {Decimal}
+ * @return {Boolean}
+ */
+Decimal.prototype.isNaN = function() {};
+
+/**
+ * @this {Decimal}
+ * @return {Boolean}
+ */
+Decimal.prototype.isNegative = function() {};
+
+/**
+ * @this {Decimal}
+ * @return {Boolean}
+ */
+Decimal.prototype.isNeg = function() {};
+
+/**
+ * @this {Decimal}
+ * @return {Boolean}
+ */
+Decimal.prototype.isPositive = function() {};
+
+/**
+ * @this {Decimal}
+ * @return {Boolean}
+ */
+Decimal.prototype.isPos = function() {};
+
+/**
+ * @this {Decimal}
+ * @return {Boolean}
+ */
+Decimal.prototype.lessThan = function() {};
+
+/**
+ * @this {Decimal}
+ * @return {Boolean}
+ */
+Decimal.prototype.lt = function() {};
+
+/**
+ * @this {Decimal}
+ * @return {Boolean}
+ */
+Decimal.prototype.lessThanOrEqualTo = function() {};
+
+/**
+ * @this {Decimal}
+ * @return {Boolean}
+ */
+Decimal.prototype.lte = function() {};
+
+/**
+ * @this {Decimal}
+ * @return {Decimal}
+ */
+Decimal.prototype.logarithm = function() {};
+
+/**
+ * @this {Decimal}
+ * @return {Decimal}
+ */
+Decimal.prototype.log = function() {};
+
+/**
+ * @this {Decimal}
+ * @return {Decimal}
+ */
+Decimal.prototype.minus = function() {};
+
+/**
+ * @this {Decimal}
+ * @return {Decimal}
+ */
+Decimal.prototype.modulo = function() {};
+
+/**
+ * @this {Decimal}
+ * @return {Decimal}
+ */
+Decimal.prototype.mod = function() {};
+
+/**
+ * @this {Decimal}
+ * @return {Decimal}
+ */
+Decimal.prototype.naturalExponential = function() {};
+
+/**
+ * @this {Decimal}
+ * @return {Decimal}
+ */
+Decimal.prototype.exp = function() {};
+
+/**
+ * @this {Decimal}
+ * @return {Decimal}
+ */
+Decimal.prototype.naturalLogarithm = function() {};
+
+/**
+ * @this {Decimal}
+ * @return {Decimal}
+ */
+Decimal.prototype.ln = function() {};
+
+/**
+ * @this {Decimal}
+ * @return {Decimal}
+ */
+Decimal.prototype.negated = function() {};
+
+/**
+ * @this {Decimal}
+ * @return {Decimal}
+ */
+Decimal.prototype.neg = function() {};
+
+/**
+ * @this {Decimal}
+ * @return {Decimal}
+ */
+Decimal.prototype.plus = function() {};
+
+/**
+ * @this {Decimal}
+ * @return {number}
+ */
+Decimal.prototype.precision = function() {};
+
+/**
+ * @this {Decimal}
+ * @return {number}
+ */
+Decimal.prototype.sd = function() {};
+
+/**
+ * @this {Decimal}
+ * @return {Decimal}
+ */
+Decimal.prototype.round = function() {};
+
+/**
+ * @this {Decimal}
+ * @return {Decimal}
+ */
+Decimal.prototype.round = function() {};
+
+/**
+ * @this {Decimal}
+ * @return {Decimal}
+ */
+Decimal.prototype.sine = function() {};
+
+/**
+ * @this {Decimal}
+ * @return {Decimal}
+ */
+Decimal.prototype.sin = function() {};
+
+/**
+ * @this {Decimal}
+ * @return {Decimal}
+ */
+Decimal.prototype.squareRoot = function() {};
+
+/**
+ * @this {Decimal}
+ * @return {Decimal}
+ */
+Decimal.prototype.sqrt = function() {};
+
+/**
+ * @this {Decimal}
+ * @return {Decimal}
+ */
+Decimal.prototype.tangent = function() {};
+
+/**
+ * @this {Decimal}
+ * @return {Decimal}
+ */
+Decimal.prototype.tan = function() {};
+
+/**
+ * @this {Decimal}
+ * @return {Decimal}
+ */
+Decimal.prototype.times = function() {};
+
+/**
+ * @this {Decimal}
+ * @return {string}
+ */
+Decimal.prototype.toBinary = function() {};
+
+/**
+ * @this {Decimal}
+ * @return {Decimal}
+ */
+Decimal.prototype.toDecimalPlaces = function() {};
+
+/**
+ * @this {Decimal}
+ * @return {Decimal}
+ */
+Decimal.prototype.toDP = function() {};
+
+/**
+ * @this {Decimal}
+ * @return {string}
+ */
+Decimal.prototype.toExponential = function() {};
+
+/**
+ * @this {Decimal}
+ * @return {string}
+ */
+Decimal.prototype.toFixed = function() {};
+
+/**
+ * @this {Decimal}
+ * @return {Array<Decimal>}
+ */
+Decimal.prototype.toFraction = function() {};
+
+/**
+ * @this {Decimal}
+ * @return {string}
+ */
+Decimal.prototype.toHexadecimal = function() {};
+
+/**
+ * @this {Decimal}
+ * @return {string}
+ */
+Decimal.prototype.toJSON = function() {};
+
+/**
+ * @this {Decimal}
+ * @return {Decimal}
+ */
+Decimal.prototype.toNearest = function() {};
+
+/**
+ * @this {Decimal}
+ * @return {number}
+ */
+Decimal.prototype.toNumber = function() {};
+
+/**
+ * @this {Decimal}
+ * @return {string}
+ */
+Decimal.prototype.toOctal = function() {};
+
+/**
+ * @this {Decimal}
+ * @return {Decimal}
+ */
+Decimal.prototype.toPower = function() {};
+
+/**
+ * @this {Decimal}
+ * @return {Decimal}
+ */
+Decimal.prototype.pow = function() {};
+
+/**
+ * @this {Decimal}
+ * @return {string}
+ */
+Decimal.prototype.toPrecision = function() {};
+
+/**
+ * @this {Decimal}
+ * @return {Decimal}
+ */
+Decimal.prototype.toSignificantDigits = function() {};
+
+/**
+ * @this {Decimal}
+ * @return {Decimal}
+ */
+Decimal.prototype.toSD = function() {};
+
+/**
+ * @this {Decimal}
+ * @return {string}
+ */
+Decimal.prototype.toString = function() {};
+
+/**
+ * @this {Decimal}
+ * @return {Decimal}
+ */
+Decimal.prototype.truncated = function() {};
+
+/**
+ * @this {Decimal}
+ * @return {Decimal}
+ */
+Decimal.prototype.trunc = function() {};
+
+/**
+ * @this {Decimal}
+ * @return {string}
+ */
+Decimal.prototype.valueOf = function() {};
+
+/**
+ * @this {null}
+ * @return {Decimal}
+ */
+Decimal.abs = function() {};
+
+/**
+ * @this {null}
+ * @return {Decimal}
+ */
+Decimal.acos = function() {};
+
+/**
+ * @this {null}
+ * @return {Decimal}
+ */
+Decimal.acosh = function() {};
+
+/**
+ * @this {null}
+ * @return {Decimal}
+ */
+Decimal.add = function() {};
+
+/**
+ * @this {null}
+ * @return {Decimal}
+ */
+Decimal.asin = function() {};
+
+/**
+ * @this {null}
+ * @return {Decimal}
+ */
+Decimal.asinh = function() {};
+
+/**
+ * @this {null}
+ * @return {Decimal}
+ */
+Decimal.atan = function() {};
+
+/**
+ * @this {null}
+ * @return {Decimal}
+ */
+Decimal.atanh = function() {};
+
+/**
+ * @this {null}
+ * @return {Decimal}
+ */
+Decimal.atan2 = function() {};
+
+/**
+ * @this {null}
+ * @return {Decimal}
+ */
+Decimal.cbrt = function() {};
+
+/**
+ * @this {null}
+ * @return {Decimal}
+ */
+Decimal.ceil = function() {};
+
+/**
+ * @this {null}
+ * @return {}
+ */
+Decimal.clone = function() {};
+
+/**
+ * @this {null}
+ * @return {null}
+ */
+Decimal.config = function() {};
+
+/**
+ * @this {null}
+ * @return {Decimal}
+ */
+Decimal.cos = function() {};
+
+/**
+ * @this {null}
+ * @return {Decimal}
+ */
+Decimal.cosh = function() {};
+
+/**
+ * @this {null}
+ * @return {Decimal}
+ */
+Decimal.div = function() {};
+
+/**
+ * @this {null}
+ * @return {Decimal}
+ */
+Decimal.exp = function() {};
+
+/**
+ * @this {null}
+ * @return {Decimal}
+ */
+Decimal.floor = function() {};
+
+/**
+ * @this {null}
+ * @return {Decimal}
+ */
+Decimal.fromJSON = function() {};
+
+/**
+ * @this {null}
+ * @return {Decimal}
+ */
+Decimal.hypot = function() {};
+
+/**
+ * @this {null}
+ * @return {Decimal}
+ */
+Decimal.ln = function() {};
+
+/**
+ * @this {null}
+ * @return {Decimal}
+ */
+Decimal.log = function() {};
+
+/**
+ * @this {null}
+ * @return {Decimal}
+ */
+Decimal.log2 = function() {};
+
+/**
+ * @this {null}
+ * @return {Decimal}
+ */
+Decimal.log10 = function() {};
+
+/**
+ * @this {null}
+ * @return {Decimal}
+ */
+Decimal.max = function() {};
+
+/**
+ * @this {null}
+ * @return {Decimal}
+ */
+Decimal.min = function() {};
+
+/**
+ * @this {null}
+ * @return {Decimal}
+ */
+Decimal.mod = function() {};
+
+/**
+ * @this {null}
+ * @return {Decimal}
+ */
+Decimal.mul = function() {};
+
+/**
+ * @this {null}
+ */
+Decimal.noConflict = function() {};
+
+/**
+ * @this {null}
+ * @return {Decimal}
+ */
+Decimal.pow = function() {};
+
+/**
+ * @this {null}
+ * @return {Decimal}
+ */
+Decimal.random = function() {};
+
+/**
+ * @this {null}
+ * @return {Decimal}
+ */
+Decimal.round = function() {};
+
+/**
+ * @this {null}
+ * @return {number}
+ */
+Decimal.sign = function() {};
+
+
