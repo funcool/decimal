908d65c1c6e223bcaba9eafd8209013916e3228e
Jesús Espino <jespinog@gmail.com>
2017-03-10 12:59:30 +0100

    Fixing license in project.clj

--- project.clj
+++ project.clj
@@ -1,7 +1,8 @@
 (defproject funcool/decimal "0.1.0"
   :description "An arbitrary-precision Decimal type for ClojureScript."
   :url "https://github.com/funcool/decimal"
-  :license {:name "Public Domain" :url "http://unlicense.org/"}
+  :license {:name "BSD (2-Clause)"
+            :url "http://opensource.org/licenses/BSD-2-Clause"}
   :dependencies [[org.clojure/clojure "1.8.0" :scope "provided"]
                  [org.clojure/clojurescript "1.7.228" :scope "provided"]]
   :deploy-repositories {"releases" :clojars
