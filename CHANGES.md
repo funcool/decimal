4d99f75b0ede75d801ee33a14e3109715bcceef5
Andrey Antukh <niwi@niwi.nz>
2017-03-21 19:45:06 +0100

    Set version to 1.0.1

--- CHANGES.md
+++ CHANGES.md
@@ -1,6 +1,13 @@
 # Changelog #
 
-## Version 1.0.0
+## Version 1.0.1 ##
+
+Date: 2017-03-21
+
+- Fix an issue with the decimal.js bundle.
+
+
+## Version 1.0.0 ##
 
 Date: 2017-03-10
 
--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject funcool/decimal "1.0.0"
+(defproject funcool/decimal "1.0.1"
   :description "An arbitrary-precision Decimal type for ClojureScript."
   :url "https://github.com/funcool/decimal"
   :license {:name "BSD (2-Clause)"
